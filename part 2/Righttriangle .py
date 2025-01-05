5e1d8419993554f47e06b91d058008ccd151637c
JacoboOquendo0222 <joquendo1@students.tufsd.org>
2025-01-05 10:57:29 -0500

    Add files via upload

--- part 2/Righttriangle .py
+++ part 2/Righttriangle .py
@@ -1,13 +1,13 @@
 '''
-*****
-*****
-*****
-*****
+*
+**
+***
+****
 *****
 '''
-print("The Size Of The Square?")
+print("The Size Of The Right Triangle?")
 Size=int(input())
 for i in range(Size):
-    for e in range(Size):
+    for e in range(i+1):
         print("*",end="")
     print()
