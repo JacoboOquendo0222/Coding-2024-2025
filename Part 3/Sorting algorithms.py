3d5efab6afc058dcba364bdfa518879c9279b081
Jacobo <joquendo1@students.tufsd.org>
2025-05-04 09:56:49 -0400

    we made more Sorting

--- Part 3/Sorting algorithms.py
+++ Part 3/Sorting algorithms.py
@@ -1,7 +1,12 @@
 def insertion_sort(placements):
     for io in range(0,len(placements)):
-        finding=io
-        for iae in range(io,-0,-1)
+        for iae in range(io,-0,-1):
+            if placements[iae]<placements[iae-1]:
+                stuff=placements[iae]
+                placements[iae]=placements[iae-1]
+                placements[iae-1]=stuff
+            else:
+                break    
 def sorting(placements):
     for o in range(0,len(placements)):
         finding=o
@@ -11,6 +16,15 @@ def sorting(placements):
         stuff=placements[o]
         placements[o]=placements[finding]
         placements[finding]=stuff
+def bubble_sort(placements):
+    for oi in range(0,len(placements)):
+        for oie in range(0,len(placements)-1):
+            if placements[oie]>placements[oie+1]:
+                stuff=placements[oie+1]
+                placements[oie+1]= placements[oie]
+                placements[oie]=stuff
+def merge(placements):
+    for ei in range(0,len(placements))                
 storage=[]
 print("How long should the list be?")
 long=int(input())
@@ -18,5 +32,5 @@ for a in range(long):
     print("What number should be in the list?")
     number=int(input())
     storage.append(number)
-sorting(storage)
+bubble_sort(storage)
 print(storage)
