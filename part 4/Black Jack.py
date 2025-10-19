76e79c5e809ccf560ba24fa1cf64a0870c7d906b
Jacobo <joquendo1@students.tufsd.org>
2025-10-19 09:57:34 -0400

    continued blackjack

--- part 4/Black Jack.py
+++ part 4/Black Jack.py
@@ -1,4 +1,13 @@
 from Deck import deck
+def values(playerhand):
+    totalvalue=(0)
+    for card in playerhand:
+        if card.value<=10:
+            totalvalue=totalvalue+card.value
+        else:
+            card.value>10
+            totalvalue=totalvalue+10
+    return totalvalue
 clubs=deck()
 clubs.shuffle()
 playing=True
@@ -9,4 +18,9 @@ while playing==True:
     dealerhand.append(clubs.draw())
     playerhand.append(clubs.draw())
     dealerhand.append(clubs.draw())
-print("My first card is:"+dealerhand[0])
+    print("My first card is:"+str(dealerhand[0]))
+    playerturn=True
+    while playerturn==True:
+        for card in playerhand:
+            print(card)
+    break
