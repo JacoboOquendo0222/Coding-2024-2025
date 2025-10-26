c2835e036e0182ce1151a1ea745f8c6aaf90489c
Jacobo <joquendo1@students.tufsd.org>
2025-10-26 09:28:31 -0400

    continue blackjack

--- part 4/Black Jack.py
+++ part 4/Black Jack.py
@@ -18,9 +18,29 @@ while playing==True:
     dealerhand.append(clubs.draw())
     playerhand.append(clubs.draw())
     dealerhand.append(clubs.draw())
-    print("My first card is:"+str(dealerhand[0]))
+    print("My first cards is:"+str(dealerhand[0]))
     playerturn=True
+    print("Your cards are:")
     while playerturn==True:
         for card in playerhand:
             print(card)
+        value=values(playerhand)
+        print("Your value is", value)
+        if value>=21:
+            playerturn=False
+        else:
+            print("Do you want to Hit or Stand")
+            Hit=input().lower()
+            if Hit=="hit":
+                playerhand.append(clubs.draw())
+            if Hit=="stand":
+                playerturn=False
+    while values(dealerhand)<17:
+        dealerhand.append(clubs.draw())
+    for card in dealerhand:
+        print(card)
+    dealervalue=values(dealerhand)
+    if dealervalue>21:
+        if value>21:
+            print("Tie")
     break
