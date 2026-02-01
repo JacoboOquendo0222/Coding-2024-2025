c1d190d418bfa90dc910b60a79435326d3f42728
Jacobo <joquendo1@students.tufsd.org>
2026-02-01 09:59:48 -0500

    finish linklist

---
+++ Part 5/Linklist tester.py
@@ -0,0 +1,7 @@
+from linklist import linklist
+listing=linklist()
+listing.add(position=0,value=921)
+listing.add(position=0,value=3)
+listing.add(position=0,value=54)
+listing.add(position=0,value=2)
+print(listing)
--- Part 5/linklist.py
+++ Part 5/linklist.py
@@ -6,8 +6,26 @@ class linklist:
         self.head=None
     def __len__(self):
         return self.size
+    def getnode(self,position):
+        start=self.head
+        for i in range(position):
+            start=node.nextnode(start)
+        return start 
     def addnode(self,position,nodes):
         if position==0:
             node.setnextnode(nodes,self.head)  
             self.head=nodes
-
+        else:
+            add=node.getnode(self,position-1)
+            node.setnextnode(nodes,add.nextnode())
+            node.setnextnode(add,nodes)
+    def add(self,position,value):
+        adding=node(value)
+        self.addnode(position,adding)
+    def __str__(self):
+        rode=""
+        start=self.head
+        for i in range(self.size):
+            rode=rode+str(start)
+            start=node.nextnode(start)
+        return rode
